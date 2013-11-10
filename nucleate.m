b2c77dd8d851773b98b2b2121cafa92b2695409e
Andrew Stershic <andybond13@yahoo.com>
2013-11-09 22:52:03 -0500

    Update nucleation function to take a vector of places to nucleate localizations

--- nucleate.m
+++ nucleate.m
@@ -1,17 +1,28 @@
 function[phi] = nucleate(x,phi,xnuc,phinuc)
+%x      -mesh
+%phi    -level-set calculated at this time-step
+%xnuc   -location(s) of localizations to be nucleated
+%phinuc -amount of the level-set to be set at nucleated localizations
 
-h = 0;
-loc = 0;
-for i=1:length(x)-1
-    if ((xnuc >= x(i)) && (xnuc < x(i+1)))
-        loc = i;
-        h = x(i+1)-x(i);
-        delta = (xnuc - x(i))/h;
-        break;
+assert(length(xnuc) == length(phinuc));
+assert(length(xnuc) > 0);
+
+for j = 1:length(xnuc)
+    
+    h = 0;
+    loc = 0;
+    for i=1:length(x)-1
+        if ((xnuc(j) >= x(i)) && (xnuc(j) < x(i+1)))
+            loc = i;
+            h = x(i+1)-x(i);
+            delta = (xnuc(j) - x(i))/h;
+            break;
+        end
     end
+    
+    assert(loc ~= 0);
+    
+    phi(loc) = phinuc(j) - delta*h;
+    phi(loc+1) = phinuc(j) - (1-delta)*h;
+    
 end
-
-assert(loc ~= 0);
-
-phi(loc) = phinuc - delta*h;
-phi(loc+1) = phinuc - (1-delta)*h;
